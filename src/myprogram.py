0d6cb8fbc913a2747e7cb37e494d26ea8c26dbe2
liamr2 <liamr2@cs.washington.edu>
2025-02-25 02:11:24 -0800

    Re-implemented accuracy measurement, added a debug mode

--- src/myprogram.py
+++ src/myprogram.py
@@ -5,7 +5,7 @@ import random
 import sys
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
-os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
 # From HW2
 from typing import List, Tuple, Dict, Union
@@ -25,12 +25,15 @@ idx_to_char = {0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g', 7: 'h', 8
                25: 'z', 26: ' ', 27: '!', 28: '"', 29: '#', 30: '$', 31: '%', 32: '&',
                33: "'", 34: '(', 35: ')', 36: '*', 37: '+', 38: ',', 39: '-', 40: '.',
                41: '/', 42: '<unk>'}
-            #, 43: '1', 44: '2', 45: '3', 46: '4', 47: '5', 48: '6',49: '7', 50: '8', 51: '9', 52: '0'}
+
+
+#, 43: '1', 44: '2', 45: '3', 46: '4', 47: '5', 48: '6',49: '7', 50: '8', 51: '9', 52: '0'}
+
 
 # Actual model
 class MyModel(nn.Module):
 
-    def __init__(self, sentence_dim: int, word_dim: int, hidden_dim: int, output_dim: int):
+    def __init__(self, sentence_dim: int, word_dim: int, hidden_dim: int, output_dim: int, debug: bool = False):
         """
         Initialize the Feed-Foward model.
         Inputs:
@@ -50,6 +53,7 @@ class MyModel(nn.Module):
         self.fc1 = torch.nn.Linear(sentence_dim + word_dim, hidden_dim)
         self.act1 = torch.nn.ReLU()
         self.fc2 = torch.nn.Linear(hidden_dim, output_dim)
+        self.debug = debug
 
     def forward(self, context: torch.Tensor, curr_word: torch.Tensor) -> torch.Tensor:
         """
@@ -80,7 +84,7 @@ class MyModel(nn.Module):
             splits = entry.split()
             merged = ' '.join(splits[:-1])
             context_embedded = dataloader.get_st_embeddings([merged], st_model)  # Get context embeddings
-            word = splits[-1] # Empty string in the case that the last character is a whitespace
+            word = splits[-1]  # Empty string in the case that the last character is a whitespace
             word_embedded = dataloader.get_word_embeddings(word)
             total_embeddings.append({'context': context_embedded, 'word': word_embedded})
 
@@ -92,9 +96,9 @@ class MyModel(nn.Module):
     @classmethod
     def load_test_data(cls, fname):
         data = []
-        with open(fname, 'r', encoding='utf-8') as f: 
+        with open(fname, 'r', encoding='utf-8') as f:
             for line in f:
-                line = line.replace("’","'")
+                line = line.replace("’", "'")
                 inp = line[:-1]  # the last character is a newline
                 data.append(inp)
         return data
@@ -112,9 +116,10 @@ class MyModel(nn.Module):
         eval_batch_size: int = 128
         n_epochs: int = 100
 
-        print("Beginning Training with Parameters:\n---Learning Rate: {l}"
-              "\n---Batch Size: {b}\n---Eval Batch Size: {e}\n---Epochs: {E}\n"
-              .format(l=lr, b=batch_size, e=eval_batch_size, E=n_epochs))
+        if self.debug:
+            print("Beginning Training with Parameters:\n---Learning Rate: {l}"
+                  "\n---Batch Size: {b}\n---Eval Batch Size: {e}\n---Epochs: {E}\n"
+                  .format(l=lr, b=batch_size, e=eval_batch_size, E=n_epochs))
 
         train_loader = data
 
@@ -124,8 +129,10 @@ class MyModel(nn.Module):
         progress = 0
 
         for epoch in range(n_epochs):  # Iterate over the epochs
-            sys.stdout.write('\r[{0}{1} {2}%] Epoch: {3}'.format('#' * (int(progress / 10)),
-                             '-' * (int(n_epochs / 10) - int(progress / 10)), progress, epoch))
+            if self.debug:
+                sys.stdout.write('\r[{0}{1} {2}%] Epoch: {3}'.format('#' * (int(progress / 10)),
+                                                                     '-' * (int(n_epochs / 10) - int(progress / 10)),
+                                                                     progress, epoch))
 
             model.train()  # Set the model to training mode
             for batch in train_loader:  # Iterate over the batches of the training data
@@ -156,7 +163,6 @@ class MyModel(nn.Module):
         preds = []
         st_model = SentenceTransformer("all-mpnet-base-v2")
         for inp in data:
-            # print(inp)
             split = inp.split()
             if len(split) > 1:
                 sentence = ' '.join(split[:-1])
@@ -170,8 +176,17 @@ class MyModel(nn.Module):
             (_, top_indices) = torch.topk(logits, k=3)
             top_chars = [idx_to_char[i.item()] for i in top_indices[0]]
             preds.append(''.join(list(top_chars)))
-        # print(preds)
         return preds
+
+    def eval_preds(self, preds, labels) -> float:
+        correct = 0.0
+        total = len(preds)
+        print(len(preds), len(labels))
+        assert (len(preds) == len(labels))
+        for _pred, _label in zip(preds, labels):
+            if _label in _pred:
+                correct += 1.0
+        return correct / total
 
     def save(self, work_dir):
         # Save the model
@@ -193,11 +208,6 @@ class MyModel(nn.Module):
         nn_model.load_state_dict(torch.load(f'{work_dir}/model.checkpoint'))
         return nn_model
 
-        # this particular model has nothing to load, but for demonstration purposes we will load a blank file
-        with open(os.path.join(work_dir, 'model.checkpoint')) as f:
-            dummy_save = f.read()
-        return MyModel()
-
 
 if __name__ == '__main__':
     parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
@@ -205,6 +215,7 @@ if __name__ == '__main__':
     parser.add_argument('--work_dir', help='where to save', default='work')
     parser.add_argument('--test_data', help='path to test data', default='example/input.txt')
     parser.add_argument('--test_output', help='path to write test predictions', default='pred.txt')
+    parser.add_argument('--debug_mode', default=False)
     args = parser.parse_args()
 
     random.seed(0)
@@ -214,7 +225,7 @@ if __name__ == '__main__':
             print('Making working directory {}'.format(args.work_dir))
             os.makedirs(args.work_dir)
         print('Instantiating model')
-        model = MyModel(768, 43, 200, 43)
+        model = MyModel(768, 43, 200, 43, debug=args.debug_mode)
         print('Loading training data')
         train_data = MyModel.load_training_data()
         print('Training')
@@ -231,5 +242,9 @@ if __name__ == '__main__':
         print('Writing predictions to {}'.format(args.test_output))
         assert len(pred) == len(test_data), 'Expected {} predictions but got {}'.format(len(test_data), len(pred))
         model.write_pred(pred, args.test_output)
+        if args.debug_mode:
+            with open("data_new/dev_labels.txt") as f:  #CURRENTLY HARDCODED!
+                accuracy = model.eval_preds(pred, f.read().splitlines())
+                print(f"Accuracy: {accuracy}")
     else:
         raise NotImplementedError('Unknown mode {}'.format(args.mode))
