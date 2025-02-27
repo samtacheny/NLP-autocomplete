b4227eaeaf921d84f4d9ddbb42f9a599d734d1a5
samtach <samtach@uw.edu>
2025-02-26 19:43:00 -0800

    Removed symbols

--- datasetpreprocessor.py
+++ datasetpreprocessor.py
@@ -13,7 +13,7 @@ def load_data(languages):
         # datasets.append(load_dataset("wikipedia", "20220301.simple", trust_remote_code=True))
     return datasets
 
-def cutoff_sentence(dataset, reference, num_train_sentences, num_dev_sentences, sentence_per_article):
+def cutoff_sentence(dataset, symbols, reference, num_train_sentences, num_dev_sentences, sentence_per_article):
     dataset = dataset.shuffle()
     train_valid = dataset['train'].train_test_split(test_size=0.2)
     train_data = train_valid['train']
@@ -34,15 +34,20 @@ def cutoff_sentence(dataset, reference, num_train_sentences, num_dev_sentences, 
         sentences = text.split(".")
         article_counter = 0
         for s in sentences:
+            s = str(s)
             # Ignore sentences with lots of numbers (throws off the flow once removed)
             num_numbers = len(re.findall('[0-9]', s))
             if num_numbers > 4:
                  continue
-            s = re.sub('[0-9]+', ' ', s) # Remove numbers
-            s = re.sub('\n+', '', str(s)) # Remove newlines
+            s = re.sub('[0-9]+', ' ', str(s)) # Remove numbers
+            # Replace quotations
+            s = re.sub('[‘’]', '\'', s) # Replace quotation marks
+            s = re.sub("[“”„]", '"', s)
+            for sym in symbols: # Remove symbols
+                 s = s.replace(sym, '')
+            s = re.sub('\n+', '', s) # Remove newlines
             s = re.sub("[\u200b\xa0 ]+", ' ', s) # Standardize spaces
             s = s.strip() # Strip leading whitespace
-            # print(s)
             sen_len = len(s)
             if sen_len > 10 and s[0].isupper(): # Keep only longish sentences
                 cutoff = np.random.randint(5, sen_len)
@@ -69,15 +74,20 @@ def cutoff_sentence(dataset, reference, num_train_sentences, num_dev_sentences, 
         sentences = text.split(".") # Include other forms of punctuation?
         article_counter = 0
         for s in sentences:
+            s = str(s)
             # Ignore sentences with lots of numbers (throws off the flow once removed)
             num_numbers = len(re.findall('[0-9]', s))
             if num_numbers > 4:
                  continue
             s = re.sub('[0-9]+', ' ', s) # Remove numbers
-            s = re.sub('\n+', '', str(s)) # Remove newlines
+            # Replace quotations
+            s = re.sub('[‘’]', '\'', s) # Replace quotation marks
+            s = re.sub("[“”„]", '"', s)
+            for sym in symbols: # Remove symbols
+                 s = s.replace(sym, '')
+            s = re.sub('\n+', '', s) # Remove newlines
             s = re.sub("[\u200b\xa0 ]+", ' ', s) # Standardize spaces
             s = s.strip() # Strip leading whitespace
-            # print(s)
             sen_len = len(s)
             if sen_len > 10 and s[0].isupper(): # Keep only longish sentences
                 cutoff = np.random.randint(5, sen_len)
@@ -105,6 +115,7 @@ sentence_per_article = 10 # Number of train sentences taken per article
 languages = ['es', 'fr', 'en', 'de', 'ceb', 'pl', 'sv'] # Spanish, French, English, German, Cebuano, Polish, Swedish
 train_length = [1500, 1500, 1500, 1500, 500, 500, 500] * sentence_per_article
 references = [['Referencias'], ['Notes et références'], ['References'], ['Literatur'], [], ['Przypisy'], ['Noter', 'Källor', 'Referenser']]
+symbols = ['«', '»', '°', '(', ')', '-', ';', '–', '!', '|', '/', '+', '[', ']', '*', '—', '=', '#', '$', '.']
 
 def main():
     print('Loading Data')
@@ -121,7 +132,8 @@ def main():
         reference = references[i] # List of words to cut out references
         num_train_sentences = train_length[i] # Number of sentences for training
         num_dev_sentences = 0.2 * num_train_sentences # Number of sentences for validation
-        t_sent, t_lab, d_sent, d_lab = cutoff_sentence(dataset, reference, num_train_sentences, num_dev_sentences, sentence_per_article)
+        t_sent, t_lab, d_sent, d_lab = cutoff_sentence(dataset, symbols, reference, 
+                                                       num_train_sentences, num_dev_sentences, sentence_per_article)
         train_sentences = np.append(train_sentences, t_sent)
         train_labels = np.append(train_labels, t_lab)
         dev_sentences = np.append(dev_sentences, d_sent)
